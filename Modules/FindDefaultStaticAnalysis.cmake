0f6eb1792fea667c23839d194cd8d3a82d7fc253
Paul Helter <paulheltera@gmail.com>
2021-03-13 17:34:59 -0800

    Fixing up for latest static analysis checks.

--- Modules/FindDefaultStaticAnalysis.cmake
+++ Modules/FindDefaultStaticAnalysis.cmake
@@ -1,11 +1,13 @@
 # Copyright (c) 2017 Paul Helter
-# add a target to generate API documentation with Doxygen
+# https://blog.kitware.com/static-checks-with-cmake-cdash-iwyu-clang-tidy-lwyu-cpplint-and-cppcheck/
+
+option(STATIC_ANALYSIS "Use Static Analysis tools." OFF)
 
 if(STATIC_ANALYSIS)
     # This is used to add targets like third-party libraries that we want to bypass static analysis.
     add_custom_target(BypassStaticAnalysis)
 
-    option(USE_CLANG_TIDY "Use Clang Tidy to view any inefficiencies or good practices." OFF)
+    option(USE_CLANG_TIDY "Use Clang Tidy to view any inefficiencies or good practices." ON)
 
     if (USE_CLANG_TIDY)
       find_program(CLANG_TIDY 
@@ -16,8 +18,9 @@ if(STATIC_ANALYSIS)
             clang-tidy-6.0
             clang-tidy-4.0
       )
+      message(STATUS "CLANG_TIDY = ${CLANG_TIDY}")
       if(NOT CLANG_TIDY)
-            message(WARNING "Could not find clang_tidy, must be installed to check for clang tidy issues.")
+            message(WARNING "Could not find clang_tidy, must be installed to perform static checks.")
         else()
             option(CLANG_TIDY_FIX "Perform fixes for Clang-Tidy" OFF)
 
@@ -31,23 +34,53 @@ if(STATIC_ANALYSIS)
             endif()
         endif()
     endif()
-    
     # Clang-Tidy None file for build dir /_deps - anything third-party or grabbed from elsewhere don't do clang tidy on.
     configure_file( "${CMAKE_CURRENT_LIST_DIR}/StaticAnalysis/.clang-tidy.none.in"
                     "${CMAKE_BINARY_DIR}/_deps/.clang-tidy" COPYONLY )
     
+    # CppCheck - http://cppcheck.sourceforge.net/
+    option(USE_CPPCHECK "Use CPP check to view any inefficiencies or good practices." OFF)
+    if (USE_CPPCHECK)
+        find_program(CPPCHECK
+        NAMES
+            cppcheck
+            CppCheck
+        )
+        if(NOT CPPCHECK)
+            message(WARNING "Could not find cppcheck, must be installed to perform static checks.")
+        else()
+            set(CMAKE_CXX_CPPCHECK ${CPPCHECK} "--std=c++11")
+        endif()
+    endif(USE_CPPCHECK)
+                
     #Google style checks:
-    #set(CMAKE_CXX_CPPLINT )
+    # https://github.com/cpplint/cpplint
+    option(USE_CPPLINT "Use CPP Lint to view any inefficiencies or good practices." OFF)
+    if (USE_CPPLINT)
+        find_program(CPPLINT
+          NAMES
+            cpplint
+        )
+        if(NOT CPPLINT)
+            message(WARNING "Could not find clang_tidy, must be installed to perform static checks.")
+        else()
+            set(CMAKE_CXX_CPPLINT ${CPPLINT} "--linelength=200")
+        endif()
+    endif(USE_CPPLINT)
 
-    if ( 0 )    
-    find_program(INCLUDE_WHAT_YOU_USE iwyu)
-    if( NOT INCLUDE_WHAT_YOU_USE)
-        message(WARNING "Could not find include-what-you-use iwyu, must be installed to perform static checks.")
-    else()
-         set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE include-what-you-use -Xiwyu --verbose=3)
-         set(CMAKE_C_INCLUDE_WHAT_YOU_USE include-what-you-use -Xiwyu --verbose=3)
-#         set(CMAKE_LINK_WHAT_YOU_USE TRUE)
-    endif()
-    
-    endif(0)
+    # Include what you use
+    option(USE_IWYU "Use Include What You Use static analysis." OFF)
+    if (USE_IWYU)
+        find_program(INCLUDE_WHAT_YOU_USE 
+          NAMES 
+            iwyu
+            include-what-you-use
+        )
+        if( NOT INCLUDE_WHAT_YOU_USE)
+            message(WARNING "Could not find include-what-you-use iwyu, must be installed to perform static checks.")
+        else()
+             set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE include-what-you-use -Xiwyu --verbose=3)
+             set(CMAKE_C_INCLUDE_WHAT_YOU_USE include-what-you-use -Xiwyu --verbose=3)
+        endif()
+    endif(USE_IWYU)
 endif(STATIC_ANALYSIS)
