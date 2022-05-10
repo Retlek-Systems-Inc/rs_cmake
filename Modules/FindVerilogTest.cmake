5a5fb6d9e704a2f6985b2075ec74e02ff49fa5f9
Paul Helter <paulheltera@gmail.com>
2022-05-09 18:03:41 -0700

    Updating Verilog test to assume it is installed.

--- Modules/FindVerilogTest.cmake
+++ Modules/FindVerilogTest.cmake
@@ -20,27 +20,23 @@
 #
 # add a target to generate API documentation with Doxygen
 
-if(VERILOG_TEST)
+# Currently using verilator for testing.
+find_package(verilator 4.2.2 REQUIRED
+    HINTS
+    /usr/local
+    $ENV{VERILATOR_ROOT} 
+    ${VERILATOR_ROOT}
+)
 
-    # Currently using verilator for testing.
-    find_package(verilator REQUIRED) # 4.022 )
-    
-    #TODO: Wrap the verilog definitions to allow other build environments?
-    
-#    include(FetchContent)
-#    FetchContent_Declare(
-#      verilatorSource 
-#      GIT_REPOSITORY    http://git.veripool.org/git/verilator
-#      GIT_TAG           v4.0.
-#    )
-    #FetchContent_MakeAvailable(verilatorSource)
+if (NOT verilator_FOUND)
+    message(FATAL_ERROR "Verilator was not found. Either install it, or set the VERILATOR_ROOT environment variable")
+    # Install instructions here: https://verilator.org/guide/latest/install.html
+endif()
 
-    # Other options later:
+# Other options later:
 #    FetchContent_Declare(
 #      icarus 
 #      GIT_REPOSITORY   https://github.com/steveicarus/iverilog.git
 #      GIT_TAG          TBD
 #    )
 #    FetchContent_MakeAvailable(verilatorSource)
-    
-endif(VERILOG_TEST)
