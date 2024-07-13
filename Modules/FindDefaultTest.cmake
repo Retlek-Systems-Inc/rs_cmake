9a7f8e873257e969539bd1180711af81919f61f7
Paul Helter <paulheltera@gmail.com>
2024-07-12 18:45:11 -0700

    Fixing googletest tag.

--- Modules/FindDefaultTest.cmake
+++ Modules/FindDefaultTest.cmake
@@ -53,7 +53,7 @@ if(BUILD_TEST)
     include(FetchContent)
     FetchContent_Declare( googletest
       GIT_REPOSITORY    https://github.com/google/googletest.git
-      GIT_TAG           release-1.14.1
+      GIT_TAG           v1.14.1
     )
 
     # Prevent overriding the parent project's compiler/linker
