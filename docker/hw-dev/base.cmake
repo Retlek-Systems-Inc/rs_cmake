ecbe85c3ee8d5abb2cc65008ecdb6e5554c475b3
Paul Helter <paulheltera@gmail.com>
2024-07-30 15:59:11 -0700

    Prep for new version.

--- docker/hw-dev/base.cmake
+++ docker/hw-dev/base.cmake
@@ -21,7 +21,7 @@ option(VERILOG_TEST    "Support Verilog testing."   ON)
 
 FetchContent_Declare( rs_cmake
     GIT_REPOSITORY https://github.com/Retlek-Systems-Inc/rs_cmake
-    GIT_TAG        v0.4.2
+    GIT_TAG        v0.4.3
 )
 
 FetchContent_GetProperties( rs_cmake )
