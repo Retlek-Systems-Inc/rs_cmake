e3c14e982c3b0d5a58bb4627bbbe248a86c29960
Paul Helter <paulheltera@gmail.com>
2025-03-10 10:21:01 +0000

    Removing dead code for older vscode cmake variants file.

--- Init.cmake
+++ Init.cmake
@@ -151,10 +151,6 @@ endif()
 
 ###################
 # Setup for CMake Presets in CMake etc..
-# if(NOT EXISTS ${CMAKE_SOURCE_DIR}/cmake-variants.yaml)
-#     configure_file(${CMAKE_CURRENT_LIST_DIR}/External/vscode.cmake-variants.yaml
-#         ${CMAKE_SOURCE_DIR}/cmake-variants.yaml)
-# endif()
 if(NOT EXISTS ${CMAKE_SOURCE_DIR}/CMakePresets.json)
     configure_file(${CMAKE_CURRENT_LIST_DIR}/External/CMakePresets.json
         ${CMAKE_SOURCE_DIR}/CMakePresets.json)
