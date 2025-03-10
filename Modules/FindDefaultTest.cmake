bd91635a8bc6b96c55ae3f69cc1176e7d3f75432
Paul Helter <paulheltera@gmail.com>
2025-03-10 09:15:39 +0000

    Adding Exclude from all for googletest.

--- Modules/FindDefaultTest.cmake
+++ Modules/FindDefaultTest.cmake
@@ -57,12 +57,12 @@ if(BUILD_TEST)
     FetchContent_Declare( googletest
       GIT_REPOSITORY    https://github.com/google/googletest.git
       GIT_TAG           v1.16.0
+      EXCLUDE_FROM_ALL
     )
 
     # Prevent overriding the parent project's compiler/linker
     # settings on Windows
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE) 
-
 
     FetchContent_MakeAvailable(googletest)
 
