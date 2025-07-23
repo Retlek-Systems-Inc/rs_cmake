d069ee78e69f6946429b8ee98d005334a6bfbf72
Paul Helter <paulheltera@gmail.com>
2025-07-23 22:16:34 +0000

    Updating Sanitizer settings and preset options to no longer hide Msan.

--- Modules/Sanitizer.cmake
+++ Modules/Sanitizer.cmake
@@ -1,4 +1,4 @@
-# @copyright 2017-2024 Retlek Systems Inc.
+# @copyright 2017-2025 Retlek Systems Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -57,16 +57,102 @@ if (CMAKE_CXX_COMPILER_ID IN_LIST _supportedCompilers)
             -fsanitize=thread
     )
 
-    AddConfiguration( CONFIG Msan
-        BASE_CONFIG RelWithDebInfo
-        COMPILE_FLAGS
-            -fsanitize=memory
-            ${_commonSanFlags}
-            -fno-optimize-sibling-calls
-            -fsanitize-memory-track-origins=2
-        LINKER_FLAGS
-            -fsanitize=memory
-    )
+    set(_supportedMSanCompilers "Clang" "AppleClang")
+    if(CMAKE_CXX_COMPILER_ID IN_LIST _supportedMSanCompilers)
+        # Get Clang version
+        execute_process(
+            COMMAND ${CMAKE_CXX_COMPILER} --version
+            OUTPUT_VARIABLE CLANG_VERSION_OUTPUT
+        )
+        string(REGEX MATCH "version ([0-9]+)" CLANG_VERSION_MATCH "${CLANG_VERSION_OUTPUT}")
+        if(CLANG_VERSION_MATCH)
+            set(CLANG_VERSION ${CMAKE_MATCH_1})
+            
+            # Get the system architecture
+            execute_process(
+                COMMAND uname -m
+                OUTPUT_VARIABLE SYS_ARCH
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+
+            # Ask Clang for its resource directory
+            set(MSAN_LIBRARY_PATHS
+                "/usr/lib/llvm-${CLANG_VERSION}/lib/clang/${CLANG_VERSION}/lib/linux"
+                "/usr/lib/clang/${CLANG_VERSION}/lib/linux"
+                "/usr/lib64/clang/${CLANG_VERSION}/lib/linux"
+                "/usr/lib/llvm/lib/clang/${CLANG_VERSION}/lib/linux"
+            )
+            execute_process(
+                COMMAND ${CMAKE_CXX_COMPILER} -print-resource-dir
+                OUTPUT_VARIABLE CLANG_RESOURCE_DIR
+                OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+            if(CLANG_RESOURCE_DIR)
+                list(APPEND MSAN_LIBRARY_PATHS "${CLANG_RESOURCE_DIR}/lib/linux")
+            endif()
+
+            # Check common paths for MSAN runtime library
+
+            # Find MSAN runtime library
+            find_library(MSAN_RUNTIME_LIBRARY
+                NAMES "clang_rt.msan-${SYS_ARCH}"
+                PATHS ${MSAN_LIBRARY_PATHS}
+            )
+            find_library(MSAN_CXX_RUNTIME_LIBRARY
+                NAMES "clang_rt.msan_cxx-${SYS_ARCH}"
+                PATHS ${MSAN_LIBRARY_PATHS}
+            )
+
+            if(MSAN_RUNTIME_LIBRARY)               
+                get_filename_component(MSAN_RUNTIME_LIBRARY_PATH "${MSAN_RUNTIME_LIBRARY}" DIRECTORY)
+                set(MSAN_RT_LIB "-lclang_rt.msan-${SYS_ARCH}")
+            
+                set(MSAN_CXX_RT_LIB)
+                if(MSAN_CXX_RUNTIME_LIBRARY)
+                    set(MSAN_CXX_RT_LIB "-lclang_rt.msan_cxx-${SYS_ARCH}")
+                endif()
+            
+                message(STATUS "Found MSAN runtime Libraries Path: ${MSAN_RUNTIME_LIBRARY_PATH}")
+                message(STATUS "  MSAN runtime library: ${MSAN_RT_LIB}")
+                message(STATUS "  MSAN CXX runtime library: ${MSAN_CXX_RT_LIB}")
+
+                AddConfiguration( CONFIG Msan
+                    BASE_CONFIG RelWithDebInfo
+                    COMPILE_FLAGS
+                        -stdlib=libc++
+                        -fsanitize=memory
+                        ${_commonSanFlags}
+                        -fno-optimize-sibling-calls
+                        -fsanitize-memory-track-origins=2
+                    LINKER_FLAGS
+                        -stdlib=libc++
+                        -fsanitize=memory
+                        -L${MSAN_RUNTIME_LIBRARY_PATH}
+                        "${MSAN_RT_LIB}"
+                        "${MSAN_CXX_RT_LIB}"
+                )
+                set(MSAN_LINK_LIBRARIES 
+                    "-L${MSAN_RUNTIME_LIBRARY_PATH} ${MSAN_RT_LIB} ${MSAN_CXX_RT_LIB}"
+                    CACHE STRING "Link Libraries for MSAN builds."
+                    FORCE
+                )
+            else()
+                message(WARNING "MemorySanitizer runtime library not found. MSAN may not work correctly.")
+            endif()
+        endif()
+    else()
+        # No GCC Msan yet but try.
+        AddConfiguration( CONFIG Msan
+            BASE_CONFIG RelWithDebInfo
+            COMPILE_FLAGS
+                -fsanitize=memory
+                ${_commonSanFlags}
+                -fno-optimize-sibling-calls
+                -fsanitize-memory-track-origins=2
+            LINKER_FLAGS
+                -fsanitize=memory
+        )   
+    endif()
 
     AddConfiguration( CONFIG Ubsan
         BASE_CONFIG RelWithDebInfo
